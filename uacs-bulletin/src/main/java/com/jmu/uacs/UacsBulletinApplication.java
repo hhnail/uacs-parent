af3fbca35c7d5d58a1296cd9a8b9f694e1139ed1
Hhnail <779943749@qq.com>
2021-10-30 21:22:06 +0800

    [新增]bulletin模块服务正常启动

--- uacs-bulletin/src/main/java/com/jmu/uacs/UacsBulletinApplication.java
+++ uacs-bulletin/src/main/java/com/jmu/uacs/UacsBulletinApplication.java
@@ -1,8 +1,14 @@
 package com.jmu.uacs;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
+@MapperScan("com.jmu.uacs.bulletin.mapper") // 扫描dao包
+@EnableDiscoveryClient // 开启服务注册发现
 @SpringBootApplication
 public class UacsBulletinApplication {
 
---
+++ uacs-bulletin/src/main/java/com/jmu/uacs/config/AppSwaggerConfig.java
@@ -0,0 +1,10 @@
+package com.jmu.uacs.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2 // 启用swagger2文档化功能
+@SpringBootConfiguration
+public class AppSwaggerConfig {
+
+}
